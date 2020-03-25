09f487319cafe98c38ec80e089b2e30d9992b6d2
David Atkins <atki1080@gmail.com>
2020-03-25 15:45:32 -0600

    Add edge case tests

--- spec/features/user/user_can_see_github_repos_spec.rb
+++ spec/features/user/user_can_see_github_repos_spec.rb
@@ -1,13 +1,16 @@
 require 'rails_helper'
 
-describe 'As a user' do
-  describe 'from the dashboard' do
-    it 'I can see links for 5 of my github repos', :vcr do
+describe 'User' do
+  describe 'with a github token:' do
+    before(:each) do
       user = create(:user, github_token: ENV['GITHUB_TOKEN'])
+      create(:user, github_token: ENV['GITHUB_TOKEN_2'])
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       visit '/dashboard'
+    end
 
+    it 'can see links for 5 github repos', :vcr do
       within('#github') do
         expect(page).to have_css('.github_link', count: 5)
         expect(page).to have_link('mod-0')
@@ -16,6 +19,26 @@ describe 'As a user' do
         expect(page).to have_link('best_animals')
         expect(page).to have_link('git_and_gh_practice')
       end
+    end
+
+    it "can not see another user's github repos", :vcr do
+      within('#github') do
+        expect(page).to_not have_link('hello-world')
+        expect(page).to_not have_link('Mod0-S3-Practice')
+      end
+    end
+  end
+
+  describe 'without a github token:' do
+    before(:each) do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit '/dashboard'
+    end
+
+    it 'can not see links for github repos', :vcr do
+      expect(page).to_not have_css('#github')
     end
   end
 end
