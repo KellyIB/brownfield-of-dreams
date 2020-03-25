f58e7a622cbe16bc434824ca162074309e0a3ccc
David Atkins <atki1080@gmail.com>
2020-03-24 21:13:16 -0600

    Add test scaffold

--- spec/features/user/user_can_see_github_repos_spec.rb
+++ spec/features/user/user_can_see_github_repos_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
-describe 'User' do
-  it 'user can sign in', :vcr do
-    user = create(:user, github_token: ENV['GITHUB_TOKEN'])
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+describe 'As a user' do
+  describe 'from the dashboard' do
+    it 'I can see links for 5 of my github repos', :vcr do
+      user = create(:user, github_token: ENV['GITHUB_TOKEN'])
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    visit '/dashboard'
+      visit '/dashboard'
+
+      within('#github') do
+        expect(page).to have_link('Repo Name 1')
+        expect(page).to have_link('Repo Name 2')
+        expect(page).to have_link('Repo Name 3')
+        expect(page).to have_link('Repo Name 4')
+        expect(page).to have_link('Repo Name 5')
+      end
+    end
   end
 end
