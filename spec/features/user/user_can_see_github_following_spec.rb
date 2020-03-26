46b6ba20fafa6da4aee2c4d0171cf1a61ce473be
David Atkins <atki1080@gmail.com>
2020-03-25 20:17:41 -0600

    Add user github following spec

---
+++ spec/features/user/user_can_see_github_following_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'User' do
+  describe 'with a github token:' do
+    before(:each) do
+      user = create(:user, github_token: ENV['GITHUB_TOKEN'])
+      create(:user, github_token: ENV['GITHUB_TOKEN_2'])
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit '/dashboard'
+    end
+
+    it 'can see all github following', :vcr do
+      within('#github') do
+        within('#following') do
+          expect(page).to have_css('.github_link', count: 2)
+          expect(page).to have_link('rcallen89')
+          expect(page).to have_link('iEv0lv3')
+        end
+      end
+    end
+  end
+end
