acdfa0bef1a3e0f2fb1a9093c450ff9020e1f9c9
KellyIB <kellyibard@gmail.com>
2020-03-27 11:08:00 -0600

    added instance variable to prevent repeated api calls

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -6,21 +6,21 @@ class SearchFacade
 
   def get_repos(user)
     repo_data = @service.get_repo_data(user)
-    repo_data.map do |repo_attributes|
+    @repos || repo_data.map do |repo_attributes|
       Repo.new(repo_attributes)
     end
   end
 
   def get_followers(user)
     follower_data = @service.get_follower_data(user)
-    follower_data.map do |follower_attributes|
+    @followers || follower_data.map do |follower_attributes|
       Follower.new(follower_attributes)
     end
   end
 
   def get_following(user)
     following_data = @service.get_following_data(user)
-    following_data.map do |followed_attributes|
+    @following || following_data.map do |followed_attributes|
       Follower.new(followed_attributes)
     end
   end
