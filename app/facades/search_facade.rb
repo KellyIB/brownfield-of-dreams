b69bc31dbd7d90f948b851a540ef878ab82faa60
KellyIB <kellyibard@gmail.com>
2020-03-28 11:45:01 -0600

    add small refactor of search_facade

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,26 +1,27 @@
 class SearchFacade
 
-  def initialize
+  def initialize(user)
     @service = GithubService.new
+    @user = user
   end
 
-  def get_repos(user)
-    repo_data = @service.get_repo_data(user)
-    @repos || repo_data.map do |repo_attributes|
+  def get_repos
+    repo_data = @service.get_repo_data(@user)
+    @repos ||= repo_data.map do |repo_attributes|
       Repo.new(repo_attributes)
-    end
+    end.first(5)
   end
 
-  def get_followers(user)
-    follower_data = @service.get_follower_data(user)
-    @followers || follower_data.map do |follower_attributes|
+  def get_followers
+    follower_data = @service.get_follower_data(@user)
+    @followers ||= follower_data.map do |follower_attributes|
       Follower.new(follower_attributes)
     end
   end
 
-  def get_following(user)
-    following_data = @service.get_following_data(user)
-    @following || following_data.map do |followed_attributes|
+  def get_following
+    following_data = @service.get_following_data(@user)
+    @following ||= following_data.map do |followed_attributes|
       Follower.new(followed_attributes)
     end
   end
