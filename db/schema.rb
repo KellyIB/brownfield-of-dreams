17fdca7f2e362813b9259b64799338ce135cb50d
David Atkins <atki1080@gmail.com>
2020-03-29 23:52:03 -0600

    Add UserFriends to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_041858) do
+ActiveRecord::Schema.define(version: 2020_03_30_051221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,15 @@ ActiveRecord::Schema.define(version: 2020_03_30_041858) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_friends", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_user_friends_on_friend_id"
+    t.index ["user_id"], name: "index_user_friends_on_user_id"
+  end
+
   create_table "user_videos", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "video_id"
@@ -82,6 +91,7 @@ ActiveRecord::Schema.define(version: 2020_03_30_041858) do
     t.index ["tutorial_id"], name: "index_videos_on_tutorial_id"
   end
 
+  add_foreign_key "user_friends", "users"
   add_foreign_key "user_videos", "users"
   add_foreign_key "user_videos", "videos"
 end
