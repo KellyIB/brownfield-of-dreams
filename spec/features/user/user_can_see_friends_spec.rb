af00dc006ef44128412451719666cb0f24017600
David Atkins <atki1080@gmail.com>
2020-03-29 21:57:34 -0600

    Add friends spec

---
+++ spec/features/user/user_can_see_friends_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe 'User' do
+  describe 'with a github token:' do
+    before(:each) do
+      user = create(:user, github_token: 'token 123456')
+      create(:user, github_token: 'token 109824')
+
+      repo_json = File.read('spec/fixtures/github_repos.json')
+      followers_json = File.read('spec/fixtures/github_followers.json')
+      following_json = File.read('spec/fixtures/github_following.json')
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      stub_request(:get, "https://api.github.com/user/repos?direction=asc&sort=created")
+        .to_return(status: 200, body: repo_json)
+      stub_request(:get, "https://api.github.com/user/followers")
+        .to_return(status: 200, body: followers_json)
+      stub_request(:get, "https://api.github.com/user/following")
+        .to_return(status: 200, body: following_json)
+
+      visit '/dashboard'
+    end
+
+    it 'can see see a link to add friends only for registered users' do
+      within('#github') do
+        within('#followers') do
+          within('#follower-iEv0lv3') do
+            expect(page).to have_link('Add as Friend')
+          end
+          within('#follower-rcallen89') do
+            expect(page).to_not have_link('Add as Friend')
+          end
+        end
+      end
+    end
+  end
+end
