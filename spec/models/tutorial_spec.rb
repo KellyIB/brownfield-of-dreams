f1ff81d0b89a188f50bd456db5869e8ade3a2982
David Atkins <atki1080@gmail.com>
2020-03-30 00:39:48 -0600

    Add tutorial tests

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Tutorial, type: :model do
+  describe 'validations' do
+    it {should accept_nested_attributes_for :videos}
+  end
+
+  describe 'relationships' do
+    it {should have_many :videos}
+    it {should have_many :taggings}
+  end
 end
