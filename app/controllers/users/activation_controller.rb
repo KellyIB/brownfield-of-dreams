b637abdd87310bef7d5dec49435402a6c4775747
David Atkins <atki1080@gmail.com>
2020-03-31 16:08:54 -0600

    Remove comment

--- app/controllers/users/activation_controller.rb
+++ app/controllers/users/activation_controller.rb
@@ -4,7 +4,6 @@ class Users::ActivationController < ApplicationController
     user = User.find_by(confirmation_token: params[:token])
     if user
       user.activate
-      # user.update(status: 'active')
       flash[:success] = "Thank you! Your account is now activated."
     else
       flash[:error] = "Something went wrong."
