2edb45e69f756d3fcbd880247a1b65549de455df
David Atkins <atki1080@gmail.com>
2020-04-01 16:23:03 -0600

    Add edge case test

--- spec/features/user/user_can_send_email_invitations_spec.rb
+++ spec/features/user/user_can_send_email_invitations_spec.rb
@@ -9,6 +9,7 @@ describe 'Registered user' do
     followers_json = File.read('spec/fixtures/github_followers.json')
     following_json = File.read('spec/fixtures/github_following.json')
     user_json = File.read('spec/fixtures/github_user.json')
+    user_json_2 = File.read('spec/fixtures/github_user_2.json')
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     requests = ['repos?direction=asc&sort=created', 'followers', 'following']
@@ -24,20 +25,33 @@ describe 'Registered user' do
       .with(headers: {"Authorization" => "token #{user.github_token}"})
       .to_return(status: 200, body: user_json)
 
-    visit dashboard_path
-  end
+    stub_request(:get, "https://api.github.com/users/i-dont-exist")\
+      .with(headers: {"Authorization" => "token #{user.github_token}"})
+      .to_return(status: 200, body: user_json_2)
 
-  it "can send email invitations" do
+    visit dashboard_path
+
     click_link "Send an Invite"
 
     expect(current_path).to eq('/invite')
+  end
 
+  it 'can send email invitations' do
     fill_in 'github_handle', with: 'test-user'
     click_button('Send Invite')
 
     expect(current_path).to eq(dashboard_path)
 
     expect(page).to have_content("Successfully sent invite!")
+  end
+
+  it 'gets feedback when sending invitation to invalid account' do
+    fill_in 'github_handle', with: 'i-dont-exist'
+    click_button('Send Invite')
+
+    expect(current_path).to eq(dashboard_path)
+
+    expect(page).to have_content("The Github user you selected doesn't have an email address associated with their account.")
   end
 
 end
