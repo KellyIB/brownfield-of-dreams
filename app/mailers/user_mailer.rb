2c4d65d0fb944614da89cd6af8aa1aabf7c24cab
David Atkins <atki1080@gmail.com>
2020-04-01 16:19:03 -0600

    Add registration_invitation method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,4 +3,10 @@ class UserMailer < ApplicationMailer
     @user = user
     mail(to: user.email, subject: "Register your Turing Tutorials account!")
   end
+
+  def registration_invitation(sender, invitee)
+    @sender = sender
+    @invitee = invitee
+    mail(to: invitee.email, subject: "#{sender.first_name} has invited you to join Turing Tutorials!")
+  end
 end
