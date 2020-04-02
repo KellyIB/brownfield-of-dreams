8b7dbc673ac233bf6341cef1deafeee5155d33ec
KellyIB <kellyibard@gmail.com>
2020-04-01 19:44:56 -0600

    add validations to model and tests of those validations

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe Tutorial, type: :model do
   describe 'validations' do
     it {should accept_nested_attributes_for :videos}
+    it {should validate_presence_of(:title)}
+    it {should validate_presence_of(:description)}
+    it {should validate_presence_of(:thumbnail)}
   end
 
   describe 'relationships' do
