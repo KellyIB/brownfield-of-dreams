e0b4cc5107ddaa4ae8201e3e4b092c733023981a
KellyIB <kellyibard@gmail.com>
2020-04-01 22:03:02 -0600

    add filtered_videos method to filter videos from visitor view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,21 @@
 class WelcomeController < ApplicationController
   def index
+    @tutorials = filtered_videos
     if params[:tag]
-      @tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
+      @tutorials = @tutorials.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
     else
-      @tutorials = Tutorial.all.paginate(:page => params[:page], :per_page => 5)
+      @tutorials = @tutorials.paginate(:page => params[:page], :per_page => 5)
     end
   end
+
+  private
+
+    def filtered_videos
+      if current_user
+        Tutorial.all
+      else
+        Tutorial.where(classroom: false)
+      end
+    end
+
 end
