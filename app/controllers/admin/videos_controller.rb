7951b91db8f66fdfe06ac6572aa70b4c306e7238
David Atkins <atki1080@gmail.com>
2020-04-02 10:48:36 -0600

    Remove unused code

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -1,13 +1,4 @@
 class Admin::VideosController < Admin::BaseController
-  def edit
-    @video = Video.find(params[:video_id])
-  end
-
-  def update
-    video = Video.find(params[:id])
-    video.update(video_params)
-  end
-
   def create
     begin
       tutorial  = Tutorial.find(params[:tutorial_id])
@@ -18,19 +9,15 @@ class Admin::VideosController < Admin::BaseController
 
       flash[:success] = 'Successfully created video.'
     rescue StandardError
-      flash[:error] = errors.full_messages.to_sentence
+      flash[:error] = 'Unable to create video'
     end
 
     redirect_to edit_admin_tutorial_path(id: tutorial.id)
   end
 
   private
-
-  def video_params
-    params.permit(:position)
-  end
-
-  def new_video_params
-    params.require(:video).permit(:title, :description, :video_id, :thumbnail)
-  end
+  
+    def new_video_params
+      params.require(:video).permit(:title, :description, :video_id, :thumbnail)
+    end
 end
