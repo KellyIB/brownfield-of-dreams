317ae98d08a3e290e61dd63188c0c81967f0a934
David Atkins <atki1080@gmail.com>
2020-04-02 10:48:45 -0600

    Refactor 404 logic

--- app/controllers/admin/api/v1/base_controller.rb
+++ app/controllers/admin/api/v1/base_controller.rb
@@ -1,19 +1,11 @@
 class Admin::Api::V1::BaseController < ActionController::API
-  before_action :require_admin!
+  before_action :require_admin
 
-  def require_admin!
-    four_oh_four unless current_user.admin?
+  def require_admin
+    render file: '/public/404' unless current_user && current_user.admin?
   end
 
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find(session[:user_id])
-    else
-      @current_user ||= User.new
-    end
-  end
-
-  def four_oh_four
-    raise ActionController::RoutingError.new('Not Found')
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
